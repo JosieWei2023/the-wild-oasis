4fd4b9c5112a56aa1ee9aaf0e3380f1816a71c5c
Josie <tinaweizq@outlook.com>
2024-01-12 16:18:03 -0800

    Refactor Dashboard components

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -6,3 +6,13 @@ const StyledDashboardLayout = styled.div`
   grid-template-rows: auto 34rem auto;
   gap: 2.4rem;
 `;
+
+function DashboardLayout() {
+  return (
+    <StyledDashboardLayout>
+      <div>Statistic</div>
+    </StyledDashboardLayout>
+  );
+}
+
+export default DashboardLayout;
